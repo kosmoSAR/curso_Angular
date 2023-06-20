1aa0ecc3912cea3b2e2c6ac8eaccc83f083adc10
Johan Santiago Aristizabal <santiagoaristizabal.kosmetikon@gmail.com>
2023-06-20 14:12:58 -0500

    version 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Tarjeta{
+  titulo:string;
+  subtitulo:string;
+  nro?:number;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit {
+  title = "Mi primer App con Angular"
+  public ArregloTarjetas: Tarjeta[]=[]
+
+  ngOnInit(): void {
+    this.ArregloTarjetas = [
+      {titulo:"video 1",subtitulo: "subtitulo video 1"},
+      {titulo:"video 2",subtitulo: "subtitulo video 2"},
+      {titulo:"video 3",subtitulo: "subtitulo video 3"},
+    ]
+    
+  }
+}
